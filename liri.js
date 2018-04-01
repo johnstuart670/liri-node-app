257e0ed7106ebdbfa8b795557286cd30b9cc9960
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-04-01 11:29:49 -0600

    adding the APIs from the assignment

---
+++ liri.js
@@ -0,0 +1,12 @@
+// import the ENV information
+require("dotenv").config();
+// API Key information
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+// import request NPM
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
