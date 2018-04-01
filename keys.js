d1af82b6f63ee31abec8a976d66fbe930cd87009
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-04-01 11:25:13 -0600

    files initialized and ready for the start of the classwork.

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.XdNUI6aAUyh9fjQeXNNtDnaKh,
+  consumer_secret: process.env.03EA7cjzyXlMVhQPsfOMw0P13VQajdkn4WmxZsq1YuFmzZitb6,
+  access_token_key: process.env.980486615410786304-G48SzDIddS9NxAeRFBbAPLbqJHIQX5o,
+  access_token_secret: process.env.h2rexYuwylLn6buKPB7KFXZrP0ECyYwfOIGY0CyQ7ifhe
+};
+
+exports.spotify = {
+  id: process.env.e8a210015b2445bbae000eca5751ae09,
+  secret: process.env.40e3d4fee42040a0912c6335e17a7923
+};
